185f4d3118b7ca03d6125036052dac78a00f8e02
Trym Borgen <trym.borgen@norconsult.com>
2023-02-14 19:13:26 +0100

    Added git extension.

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,32 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { GitExtension } from './api/git';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+	const git = getGitExtension();
+
+	if (!git) {
+		vscode.window.showErrorMessage('Git extension not found.');
+		return;
+	}
+
+	const repo = git.getRepository(vscode.workspace.workspaceFolders![0].uri);
+
+	if (!repo) {
+		vscode.window.showErrorMessage('Git repository not found.');
+		return;
+	}
+
+	const changes = repo.state.workingTreeChanges;
+
+	if (changes.length === 0) {
+		vscode.window.showErrorMessage('No changes found.');
+		return;
+	}
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -20,6 +42,12 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+function getGitExtension() {
+    const vscodeGit = vscode.extensions.getExtension<GitExtension>('vscode.git');
+    const gitExtension = vscodeGit && vscodeGit.exports;
+    return gitExtension && gitExtension.getAPI(1);
 }
 
 // This method is called when your extension is deactivated
